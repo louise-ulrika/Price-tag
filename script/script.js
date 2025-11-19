22f23f334026e633d4accc1fb11be102cb813f75
louise-ulrika <ulrika.nicander@gmail.com>
2025-11-19 16:03:49 +0100

    ändring prompt visas nu
    nu kan man se prompt

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+let question= "What is the price you want to pay";
+let price= prompt(question);
+alert ("The price you want to pay is " + price)
+console.log ("Costumer entered price: " + price)
+
+
+
+
+ParseFloat (price);
+
+let newPrice= (59.90-(59.90*0.1));
+console.log ("The price is " + newPrice);
